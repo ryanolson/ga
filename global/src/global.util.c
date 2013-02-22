1d421620cd5928c194c38b6d873bcb9cbd69e105
David Addison <addy@cray.com>
2013-02-22 11:45:57 -0800

    Use __attribute__ to alias the ghost functions as this works with the Intel compiler

--- global/src/global.util.c
+++ global/src/global.util.c
@@ -1049,42 +1049,57 @@ DoublePrecision pnga_wtime()
     return (DoublePrecision)wtime; 
 }
 
-#pragma weak pdspgv_ = ghost_pdspgv_
 void ghost_pdspgv_(void* arg1, void* arg2, void* arg3, void* arg4,
                    void* arg5, void* arg6,
                    void* arg7, void* arg8,
                    void* arg9,
                    void* arg10, void* arg11,
                    void* arg12, void* arg13,
-                   void* arg14, void* arg15, void* arg16) 
-{ 
+                   void* arg14, void* arg15, void* arg16)
+{
      fprintf(stdout,"pdspgv_ is not implemented in this GA build.\nThis function is provided by NWChem. If you are running NWChem, check the link order and ensure pdspgv_ does not come from the GA library.");
      fflush(stdout);
      abort();
 }
+void pdspgv_(void* arg1, void* arg2, void* arg3, void* arg4,
+                   void* arg5, void* arg6,
+                   void* arg7, void* arg8,
+                   void* arg9,
+                   void* arg10, void* arg11,
+                   void* arg12, void* arg13,
+                   void* arg14, void* arg15, void* arg16) __attribute__ ((weak, alias ("ghost_pdspgv_")));
 
-#pragma weak pdspev_ = ghost_pdspev_
+
 void ghost_pdspev_(void* arg1, void* arg2, void* arg3, void* arg4,
                    void* arg5, void* arg6,
                    void* arg7, void* arg8,
                    void* arg9,
                    void* arg10, void* arg11,
                    void* arg12, void* arg13)
-{ 
+{
      fprintf(stdout,"pdspev_ is not implemented in this GA build.\nThis function is provided by NWChem. If you are running NWChem, check the link order and ensure pdspev_ does not come from the GA library.");
      fflush(stdout);
      abort();
 }
+void pdspev_(void* arg1, void* arg2, void* arg3, void* arg4,
+             void* arg5, void* arg6,
+             void* arg7, void* arg8,
+             void* arg9,
+             void* arg10, void* arg11,
+             void* arg12, void* arg13) __attribute__ ((weak, alias ("ghost_pdspev_")));
 
 
-#pragma weak fmemreq_ = ghost_fmemreq_
 void ghost_fmemreq_(void* arg1, void* arg2, void* arg3, void* arg4,
                     void* arg5, void* arg6,
                     void* arg7, void* arg8,
                     void* arg9)
-{ 
+{
      fprintf(stdout,"fmemreq_ is not implemented in this GA build.\nThis function is provided by NWChem. If you are running NWChem, check the link order and ensure fmemreq_ does not come from the GA library.");
      fflush(stdout);
      abort();
 }
+void fmemreq_(void* arg1, void* arg2, void* arg3, void* arg4,
+                    void* arg5, void* arg6,
+                    void* arg7, void* arg8,
+                    void* arg9) __attribute__ ((weak, alias ("ghost_fmemreq_")));
 
