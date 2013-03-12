01ab23e5c9ba1297d8189edf63cc42139802d638
David Addison <addy@cray.com>
2013-03-11 17:30:29 -0700

    Raised CONTIG_THRESHOLD to 144KB

--- armci/src-dmapp/armci_impl.h
+++ armci/src-dmapp/armci_impl.h
@@ -9,7 +9,7 @@
 
 #define ARMCI_DMAPP_PUT_OFFLOAD_THRESHOLD  2048 /* PUT threshold for switch from FMA to RDMA */
 #define ARMCI_DMAPP_GET_OFFLOAD_THRESHOLD  128  /* GET threshold for switch from FMA to RDMA (acc thread only) */
-#define ARMCI_REM_ACC_CONTIG_THRESHOLD     (128*1024) /* contig/1d threshold for switch to remote ACC thread */
+#define ARMCI_REM_ACC_CONTIG_THRESHOLD     (144*1024) /* contig/1d threshold for switch to remote ACC thread */
 #define ARMCI_REM_ACC_STRIDED_THRESHOLD    256  /* strided threshold for switch to remote ACC thread */
 #define MAX_NB_OUTSTANDING                 4096 /* Limit on the number of outstanding RDMAs */
 
